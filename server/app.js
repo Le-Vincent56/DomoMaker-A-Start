5d4e292e43c8a292692518f78e382150282cfe0c
Vincent Le <vdl9926@rit.edu>
2024-03-27 14:42:39 -0400

    Added redis and middleware

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 // Node imports
+require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const compression = require('compression');
@@ -8,6 +9,8 @@ const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const helmet = require('helmet');
 const session = require('express-session');
+const RedisStore = require('connect-redis').default;
+const redis = require('redis');
 
 // Local imports
 const router = require('./router.js');
@@ -15,7 +18,7 @@ const router = require('./router.js');
 // Establish port
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-// Establish database connection
+// Establish mongoDB connection
 const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1/DomoMaker';
 mongoose.connect(dbURI).catch((err) => {
   if (err) {
@@ -24,33 +27,45 @@ mongoose.connect(dbURI).catch((err) => {
   }
 });
 
-// Establish app
-const app = express();
+// Establish redis connection
+const redisClient = redis.createClient({
+  url: process.env.REDISCLOUD_URL,
+});
+redisClient.on('error', (err) => console.log('Redis Client Error', err));
 
-// Set app settings
-app.use(helmet());
-app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
-app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
-app.use(compression());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// Connect to redis then start the app
+redisClient.connect().then(() => {
+  // Establish app
+  const app = express();
 
-app.use(session({
-  key: 'sessionID', // Name of the cookie so it can be tracked
-  secret: 'Domo Arigato', // Private string used as a seed for hashing/creating unique session keys
-  resave: false, // Only send the session keyback to the database if it changes
-  saveUninitialized: false, // Prevents us from saving uninitialized session IDs to the database
-}));
+  // Set app settings
+  app.use(helmet());
+  app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
+  app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+  app.use(compression());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
 
-app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
-app.set('view engine', 'handlebars');
-app.set('views', `${__dirname}/../views`);
+  app.use(session({
+    key: 'sessionID', // Name of the cookie so it can be tracked
+    store: new RedisStore({ // Utilize our redis database to store sessionIDs
+      client: redisClient,
+    }),
+    secret: 'Domo Arigato', // Private string used as a seed for hashing/creating unique session keys
+    resave: false, // Only send the session keyback to the database if it changes
+    saveUninitialized: false, // Prevents us from saving uninitialized session IDs to the database
+  }));
 
-// Link router
-router(app);
+  app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
+  app.set('view engine', 'handlebars');
+  app.set('views', `${__dirname}/../views`);
 
-// Start app
-app.listen(port, (err) => {
-  if (err) { throw err; }
-  console.log(`Listening on port ${port}`);
+  // Link router
+  router(app);
+
+  // Start app
+  app.listen(port, (err) => {
+    if (err) { throw err; }
+    console.log(`Listening on port ${port}`);
+  });
 });
